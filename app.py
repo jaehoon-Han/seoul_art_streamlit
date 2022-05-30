9d867510784b2f34ce97662fbd5e0db9b9f0ff72
jaehoon-Han <eroel1060@daum.net>
2022-05-30 16:57:17 +0900

    add

--- app.py
+++ app.py
@@ -19,12 +19,12 @@ def main():
             x.append(df['X좌표'][i])
             y.append(df['Y좌표'][i])
 
-    map_osm = folium.Map(location = [x[15],y[15]],zoom_start=14)
+    map_osm = folium.Map(location = [x[14],y[14]],zoom_start=14)
 
     for i in range(len(x)) :
-        folium.Marker([x[i],y[i]], popup=df.iloc[i,1:3].values, icon=folium.Icon(color='red',icon='info-sign')).add_to(map_osm)
+        folium.Marker([x[i],y[i]], popup=df.iloc[i,8], icon=folium.Icon(color='red',icon='info-sign')).add_to(map_osm)
 
-    folium.CircleMarker(location=[x[15],y[15]],popup='주소',radius = 300,color="#3186cc", fill_color="#3186cc").add_to(map_osm)
+    folium.CircleMarker(location=[x[1],y[13]],popup='주소',radius = 300,color="#3186cc", fill_color="#3186cc").add_to(map_osm)
     map_osm.save('seoul_art.html')
     
     st_data = st_folium(map_osm, width = 725)
