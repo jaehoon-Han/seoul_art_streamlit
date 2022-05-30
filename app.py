2de981bbcc0fbd5a0b0f4fcdcc3c173dadbad77c
jaehoon-Han <eroel1060@daum.net>
2022-05-30 16:37:44 +0900

    100 add

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ import matplotlib.pyplot as plt
 from streamlit_folium import st_folium
 
 def main():
-    df = pd.read_csv('data/seoul_art.csv')
+    df = pd.read_csv('data/seoul_art100.csv')
  
     st.title('서울시 문화예술')
 
     x = []
     y = []
-    for i in range(771) :
+    for i in range(100) :
         if float(df['X좌표'][i]) == 0.0 or float(df['Y좌표'][i]) == 0.0 :
             pass
         else :
             x.append(df['X좌표'][i])
             y.append(df['Y좌표'][i])
 
-    map_osm = folium.Map(location = [x[13],y[13]],zoom_start=14)
+    map_osm = folium.Map(location = [x[15],y[15]],zoom_start=14)
 
     for i in range(len(x)) :
         folium.Marker([x[i],y[i]], popup=df.iloc[i,1:3].values, icon=folium.Icon(color='red',icon='info-sign')).add_to(map_osm)
 
-    folium.CircleMarker(location=[x[13],y[13]],popup='주소',radius = 300,color="#3186cc", fill_color="#3186cc").add_to(map_osm)
+    folium.CircleMarker(location=[x[15],y[15]],popup='주소',radius = 300,color="#3186cc", fill_color="#3186cc").add_to(map_osm)
     map_osm.save('seoul_art.html')
     
     st_data = st_folium(map_osm, width = 725)
